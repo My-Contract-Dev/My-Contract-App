cbd2039b05a2886b0f7cac50e894c3f64c09a191
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-09-05 19:25:11 +0300

    chore: install firebase auth

--- App.tsx
+++ App.tsx
@@ -2,15 +2,32 @@ import './global';
 import 'expo-dev-client';
 
 import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import * as Linking from 'expo-linking';
 import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
 import ConnectButton from './src/components/ConnectButton';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  // Handle user state changes
+  function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
   return (
     <NavigationContainer>
       <WalletConnectProvider
@@ -20,6 +37,7 @@ export default function App() {
         }}
       >
         <View style={styles.container}>
+          {user?.email ?? 'Not authorized'}
           <ConnectButton />
           <StatusBar style="auto" />
         </View>
--- ios/MyContractApp/AppDelegate.mm
+++ ios/MyContractApp/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -33,6 +35,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
